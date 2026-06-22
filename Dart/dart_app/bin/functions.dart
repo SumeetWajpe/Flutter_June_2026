3834fcee961db750d991d09b5b80e56261aa2660
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-06-22 11:31:32 +0530

    ✨ Named |Positional | Optional | Default parameters

--- Dart/dart_app/bin/functions.dart
+++ Dart/dart_app/bin/functions.dart
@@ -1,22 +1,32 @@
 void main() {
-  // printBook("Dr. APJ Abdul Kalam", "Wings Of Fire", 300);
+  printBook("Dr. APJ Abdul Kalam", "Wings Of Fire");
 
   // Named parameters
-  printBook(
-    numOfPages: 300,
-    author: "Dr. APJ Abdul Kalam",
-    title: "Wings of fire",
-  );
+  // printBook(
+  //   numOfPages: 300,
+  //   author: "Dr. APJ Abdul Kalam",
+  //   title: "Wings of fire",
+  // );
+
+  // printBook(numOfPages: 300, title: "India 2020", author: "Nandan Nilakeni");
 }
 
-// void printBook(String author, String title, int noOfPages) {
-//   print("$author, $title, $noOfPages");
+// Default values for positional parameters
+
+void printBook(String author, String title, [int noOfPages = 200]) {
+  print("$author, $title, $noOfPages");
+}
+
+// Named parameters
+// void printBook({
+//   required String author,
+//   required String title,
+//   required int numOfPages,
+// }) {
+//   print("$author, $title, $numOfPages");
 // }
 
-void printBook({
-  required String author,
-  required String title,
-  required int numOfPages,
-}) {
-  print("$author, $title, $numOfPages");
-}
+// Optional parameters
+// void printBook({String? author, String? title, required int numOfPages}) {
+//   print("$author, $title, $numOfPages");
+// }
