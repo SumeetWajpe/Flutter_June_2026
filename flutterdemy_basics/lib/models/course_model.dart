5710579a231c0c5d6e9e7bd88e8cc17b76ea7175
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-06-23 12:44:15 +0530

    ✨ Adding CourseModel

--- flutterdemy_basics/lib/models/course_model.dart
+++ flutterdemy_basics/lib/models/course_model.dart
@@ -1,1 +1,10 @@
 // id, title, subtitle, imageUrl
+
+class CourseModel {
+  final int id;
+  final String? title;
+  final String? subtitle;
+  final String? imageUrl;
+
+  CourseModel(this.id, this.title, this.subtitle, this.imageUrl);
+}
--- flutterdemy_basics/lib/widgets/listview_listtile.dart
+++ flutterdemy_basics/lib/widgets/listview_listtile.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutterdemy_basics/models/course_model.dart';
 
 class CoursesWithListTile extends StatelessWidget {
-  const CoursesWithListTile({super.key});
+  CoursesWithListTile({super.key});
+
+  final List<CourseModel> listofcourses = [
+    CourseModel(
+      1,
+      "React",
+      "A JS library",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/3840px-React-icon.svg.png?utm_source=commons.wikimedia.org&utm_campaign=index&utm_content=thumbnail",
+    ),
+    CourseModel(
+      2,
+      "Node",
+      "JS Server Side FX",
+      "https://miro.medium.com/v2/resize:fit:800/1*bc9pmTiyKR0WNPka2w3e0Q.png",
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
