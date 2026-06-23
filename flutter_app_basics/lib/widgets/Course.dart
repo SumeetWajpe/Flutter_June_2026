c3fa3f7406d7483197ed110b128d314586631089
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-06-23 12:15:21 +0530

    ✨ Course | With Rows And Columns

--- flutter_app_basics/lib/widgets/Course.dart
+++ flutter_app_basics/lib/widgets/Course.dart
@@ -5,9 +5,34 @@ class Courses extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Image(image: AssetImage("assets/images/react.jpg"), width: 100),
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Image(image: AssetImage("assets/images/react.jpg"), width: 100),
+            Column(
+              children: [
+                Text("React", style: TextStyle(fontSize: 25)),
+                Text("A Js Library"),
+              ],
+            ),
+            Text("Delete", style: TextStyle(color: Colors.red)),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Image(image: AssetImage("assets/images/react.jpg"), width: 100),
+            Column(
+              children: [
+                Text("React", style: TextStyle(fontSize: 25)),
+                Text("A Js Library"),
+              ],
+            ),
+            Text("Delete", style: TextStyle(color: Colors.red)),
+          ],
+        ),
       ],
     );
   }
