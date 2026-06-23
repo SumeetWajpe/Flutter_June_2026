803b0ad38a71f771e49c788c4700513fea417896
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-06-23 10:13:02 +0530

    ✨ Using Container Widget

--- flutter_app_basics/lib/main.dart
+++ flutter_app_basics/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_basics/widgets/Container.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           centerTitle: true,
@@ -20,13 +22,15 @@ class MyApp extends StatelessWidget {
           ),
           backgroundColor: Colors.blue,
         ),
-        body: Center(
-          child: Text(
-            "Hello Flutter !",
-            style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
-          ),
-        ),
+        body: MyContainer(),
       ),
     );
   }
 }
+
+//  body: Center(
+//           child: Text(
+//             "Hello Flutter !",
+//             style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+//           ),
+//         ),
---
+++ flutter_app_basics/lib/widgets/Container.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      margin: EdgeInsets.all(20),
+      height: 100,
+      width: 100,
+      color: Colors.amber,
+      child: Text("Using Container !"),
+    );
+  }
+}
